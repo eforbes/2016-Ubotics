6d984c0ea89501dc2afc14e5d8697bff1225709e
Sean <silverwolf4350@gmail.com>
2016-01-16 14:33:08 -0600

    Rename

--- src/org/usfirst/frc/team3507/robot/commands/RightTrack.java
+++ src/org/usfirst/frc/team3507/robot/commands/RightTrack.java
@@ -7,9 +7,9 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  *
  */
-public class RightSpeed extends Command {
+public class RightTrack extends Command {
 
-    public RightSpeed() {
+    public RightTrack() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.DriveTrainRight);
