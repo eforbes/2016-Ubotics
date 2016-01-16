bff69e54e1b1e3cffcc4ce005666b5d2342059bd
afortney <afortney@192.168.2.1>
2016-01-16 13:57:11 -0600

    Added left track subsystem and commands

---
+++ src/org/usfirst/frc/team3507/robot/subsystems/DriveTrainLeft.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team3507.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class DriveTrainLeft extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    public void forward(){
+    	//moves the left track forwards, accepts a value between 0 and 1
+    }
+    public void stop(){
+    	//stops the left track, value is 0
+    }
+    public void backwards(){
+    	//moves the left track backwards, accepts a value between 0 and -1
+    }
+}
+
