979bf21a9d69862c2cd9086de2c51cbb1aae94c4
Sean <silverwolf4350@gmail.com>
2016-02-06 09:27:47 -0600

    Fixes

--- src/org/usfirst/frc/team3507/robot/ImagePIDOutput.java
+++ src/org/usfirst/frc/team3507/robot/ImagePIDOutput.java
@@ -1,14 +1,11 @@
 package org.usfirst.frc.team3507.robot;
 
-import org.usfirst.frc.team3507.robot.commands.DriveTrainPID;
-
 import edu.wpi.first.wpilibj.PIDOutput;
-import edu.wpi.first.wpilibj.command.Command;
 
 public class ImagePIDOutput implements PIDOutput {
 
 	@Override
 	public void pidWrite(double output) {
-		Command d = new DriveTrainPID(output, -output);
+		Robot.driveTrain.go(output, -output);
 	}
 }
