e1ad04618ed942e1ba7e7ab53721efd32b079766
Anderson Wu <skyl_x@yahoo.com>
2016-02-18 19:24:26 -0600

    Add limit switch for arm angle.

--- src/org/usfirst/frc/team3507/robot/commands/ArmAngle.java
+++ src/org/usfirst/frc/team3507/robot/commands/ArmAngle.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team3507.robot.commands;
 
 import org.usfirst.frc.team3507.robot.Robot;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -12,7 +13,7 @@ public class ArmAngle extends Command {
 	
 	private boolean backwards;
 	Preferences prefs = Preferences.getInstance();
-	
+	DigitalInput btn = new DigitalInput(2);
     public ArmAngle(boolean b) {
     	super("ArmAngle");
     	requires(Robot.arm);
@@ -26,8 +27,13 @@ public class ArmAngle extends Command {
     	if (!backwards) {
     		Robot.arm.go(prefs.getDouble("Arm Angle Speed", 0.5));
     	} else {
-
-    		Robot.arm.go(-(prefs.getDouble("Arm Angle Speed", 0.5)));
+    		if (btn.get()){
+    			Robot.arm.stop();
+    		
+    		} else {
+        		Robot.arm.go(-(prefs.getDouble("Arm Angle Speed", 0.5)));
+    		}
+    		
     	}
     }
 
